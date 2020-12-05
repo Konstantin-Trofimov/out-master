7aceafcb25985745ed3e802b39d7905b80926e6a
Konstantim <const.trofimov@gmail.com>
2020-12-06 02:49:51 +0300

    modifired

--- src/js/script.js
+++ src/js/script.js
@@ -1,45 +1,44 @@
-'use strict';
-
-// import menu from "./modules/menu.js"; 
+'use strict'; // import menu from "./modules/menu.js"; 
 // import  search from './modules/search.js';
 // import  from './modules/';
 // import  from './modules/';
 // import  from './modules/';
 
+require("core-js/modules/es.array.for-each");
+
+require("core-js/modules/web.dom-collections.for-each");
+
 window.addEventListener('DOMContentLoaded', function () {
-
-  const menu = () => {
-    const hamburger = document.querySelector('.left-menu'),
-    menu = document.querySelector('.menu'),
-    closeElem = document.querySelector('.menu__close');
-
-    hamburger.addEventListener('click', () => {
+  var menu = function menu() {
+    var hamburger = document.querySelector('.left-menu'),
+        menu = document.querySelector('.menu'),
+        closeElem = document.querySelector('.menu__close');
+    hamburger.addEventListener('click', function () {
       menu.classList.add('active');
     });
-
-    closeElem.addEventListener('click', () => {
+    closeElem.addEventListener('click', function () {
       menu.classList.remove('active');
-    }); 
-  }   
+    });
+  };
 
-  const search = () => { 
-    const loupe = [document.querySelector('.search__wrapper'), document.querySelector('.search__input')],
-    search = document.querySelector('.search');
-    const nav = document.querySelector('.header__menu');
+  var search = function search() {
+    var loupe = [document.querySelector('.search__wrapper'), document.querySelector('.search__input')],
+        search = document.querySelector('.search');
+    var nav = document.querySelector('.header__menu');
 
-    const shift = (elem, className) => {
+    var shift = function shift(elem, className) {
       elem.classList.toggle(className);
-    }
+    };
 
-    document.querySelector('.loupe').addEventListener('click', () => {
-      loupe.forEach(i => shift(i, 'loupe_active'));
+    document.querySelector('.loupe').addEventListener('click', function () {
+      loupe.forEach(function (i) {
+        return shift(i, 'loupe_active');
+      });
       loupe[1].focus();
       shift(search, 'search_shift');
       shift(nav, 'header__menu_shift');
     });
-
-  }
- 
+  };
 
   menu();
   search();
---
+++ src/js/source.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// import menu from "./modules/menu.js"; 
+// import  search from './modules/search.js';
+// import  from './modules/';
+// import  from './modules/';
+// import  from './modules/';
+
+window.addEventListener('DOMContentLoaded', function () {
+
+  const menu = () => {
+    const hamburger = document.querySelector('.left-menu'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+
+    hamburger.addEventListener('click', () => {
+      menu.classList.add('active');
+    });
+
+    closeElem.addEventListener('click', () => {
+      menu.classList.remove('active');
+    }); 
+  }   
+
+  const search = () => { 
+    const loupe = [document.querySelector('.search__wrapper'), document.querySelector('.search__input')],
+    search = document.querySelector('.search');
+    const nav = document.querySelector('.header__menu');
+
+    const shift = (elem, className) => {
+      elem.classList.toggle(className);
+    }
+
+    document.querySelector('.loupe').addEventListener('click', () => {
+      loupe.forEach(i => shift(i, 'loupe_active'));
+      loupe[1].focus();
+      shift(search, 'search_shift');
+      shift(nav, 'header__menu_shift');
+    });
+
+  }
+ 
+
+  menu();
+  search();
+});
